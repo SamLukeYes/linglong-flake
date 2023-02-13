0a9c3378be30ffcd784a2b4327204c8fa4add64b
SamLukeYes <samlukeyes123@gmail.com>
2023-02-13 10:08:48 +0800

    Add modules

--- flake.nix
+++ flake.nix
@@ -10,5 +10,27 @@
     system = "x86_64-linux";
   in {
     packages.${system} = import ./. { pkgs = nixpkgs.legacyPackages.${system}; };
+    defaultPackage.${system} = self.packages.${system}.linglong;
+    
+    nixosModules = { config, lib, ... }:
+      with lib self.packages.${system};
+      let cfg = config.services.linglong; in
+      {
+        options = {
+          services.linglong = {
+            enable = mkEnableOption "linglong";
+          };
+        };
+
+        config = mkIf cfg.enable {
+          environment = {
+            profiles = [ "${linglong}/etc/profile.d" ];
+            systemPackages = [ linglong ];
+          };
+          services.dbus.packages = [ linglong ];
+          systemd.packages = [ linglong ];
+          users.users.deepin-linglong.isSystemUser = true;
+        };
+      };
   };
 }
